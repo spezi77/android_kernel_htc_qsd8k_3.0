81232b8f8c9018ef211a17549e45b22ac2a4f98b
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-04-09 16:55:52 -0500

    arm: kernel: remove EngineerID
    Change-Id: Ie494444426147dc233dd3733e4dce80149b2869b

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -1104,7 +1104,6 @@ static int c_show(struct seq_file *m, void *v)
 
 	seq_printf(m, "Hardware\t: %s\n", machine_name);
 	seq_printf(m, "Revision\t: %04x\n", system_rev2);
-	seq_printf(m, "EngineerID\t: %04x\n", get_engineerid());
 	seq_printf(m, "Serial\t\t: %08x%08x\n",
 		   system_serial_high, system_serial_low);
 
