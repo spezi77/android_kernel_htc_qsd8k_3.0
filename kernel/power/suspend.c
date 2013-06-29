e8096c91e0e9078102ef2baaffee79651baed2fa
NooNameR <noonamer@gmail.com>
2013-06-29 18:04:36 +0400

    bravo: fixed freezes #1

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -212,8 +212,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 		if (error)
 			goto Close;
 	}
-	if (!suspend_console_deferred)
-		suspend_console();
+	suspend_console();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
@@ -230,8 +229,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	suspend_test_start();
 	dpm_resume_end(PMSG_RESUME);
 	suspend_test_finish("resume devices");
-	if (!suspend_console_deferred)
-		resume_console();
+	resume_console();
  Close:
 	if (suspend_ops->end)
 		suspend_ops->end();
